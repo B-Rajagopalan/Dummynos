e5b1ea1f665f497d7dbd7dbf649aef2cf38ca2ac
B-Rajagopalan <rajagopalan.balamuthukumar@gmail.com>
2026-06-05 22:20:20 +0530

    feat: initialize pizza_db schema and seed initial order data

--- backend/Pizza_Producer/src/main/resources/Pizza.sql
+++ backend/Pizza_Producer/src/main/resources/Pizza.sql
@@ -16,11 +16,11 @@ bill double,customerContactNumber varchar(10), PRIMARY KEY(orderId)
 
 insert into pizza (orderId,pizzaName,quantity,bill,customerContactNumber) values
 
-  (1001,'VegSmall',1,200,"1234567890"),
+  (1001,'Margherita',1,200,"1234567890"),
 
-  (1002,'VegMedium',2,400,"9543214753"),
+  (1002,'Pepperoni',2,400,"9543214753"),
 
-  (1003,'VegLarge',1,600,"5843919283"),
+  (1003,'Veggie Supreme',3,600,"5843919283"),
 
-  (1004,'NonVegSmall',2,400,"3742828282");
+  (1004,'Four Cheese',2,400,"3742828282");
 commit;
